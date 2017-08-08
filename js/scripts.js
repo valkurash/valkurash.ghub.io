84baf7c122592159df15715528fe29cd08c57560
valkurash <valkurash@gmail.com>
2017-08-08 21:02:42 +0300

    add skills animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 $(function () {
     $('a[href*=\\#]').on('click', function (event) {
         event.preventDefault();
-        
-        if(!$(this.hash).length) return false;
-        
+
+        if (!$(this.hash).length) return false;
+
         $('html,body').animate({
-            scrollTop: $(this.hash).offset().top-$('header.main').height()
+            scrollTop: $(this.hash).offset().top - $('header.main').height()
         }, 500);
+    });
+    $(".shuffle:not(.active)").click(function () {
+        event.preventDefault();
+        $('.skill-item:not(.transform-active)').addClass('transform-active');
+        $(this).addClass('active').text('Dammit! Now help me return my skills, please.')
+    });
+    $("#about").on("click", ".transform-active>svg", function () {
+        $(this).closest('.skill-item').removeClass('transform-active');
+        if (!$('.skill-item.transform-active').length) {
+            $(".shuffle").removeClass('active').html('Thanks! <span>Do not </span>press this again!')
+        }
     });
 });
