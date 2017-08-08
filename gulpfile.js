231508ebebcd4c1b72018d84a05fa4447ed251cd
valkurash <valkurash@gmail.com>
2017-08-08 17:18:28 +0300

    add jquery lib

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+
+gulp.task('copy', function () {
+    return gulp.src('node_modules/jquery/dist/*.{js,map}')
+      .pipe(gulp.dest('js'));
+});
+
 gulp.task('sass', function() {
     gulp.src('css/*.scss')
         .pipe(sass())
